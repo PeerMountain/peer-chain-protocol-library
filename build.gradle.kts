2a8667d4b724ac38075874bc4bd9366aad487de3
Igor Drozdov <igor@kyc3.com>
2022-01-11 13:07:16 +0100

    [PEER-152] Init gradle

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.5.20"
+}
+
+group = "com.kyc3"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+}
