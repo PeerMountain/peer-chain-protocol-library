d464966d92d472d9ac3812aaeaf22efffc216fbb
Igor Drozdov <igor@kyc3.com>
2022-01-13 13:12:28 +0100

    [PEER-152] Update proto version

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ val commit = grgit.head().abbreviatedId
 version = commit
 
 dependencies {
-    implementation("com.kyc3:oracle-definitions:b220af7")
+    implementation("com.kyc3:oracle-definitions:8bfa4f3")
 
     implementation("org.igniterealtime.smack:smack-tcp:4.5.0-alpha1-SNAPSHOT")
     implementation("org.igniterealtime.smack:smack-core:4.5.0-alpha1-SNAPSHOT")
